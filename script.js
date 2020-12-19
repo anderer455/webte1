a8afd3843169ce2d07c73fb23195ca9f81f8de47
AdamT <dubst3pdnb@gmail.com>
2020-12-19 19:56:00 +0100

    menu design + logo

--- script.js
+++ script.js
@@ -81,30 +81,50 @@ document.addEventListener("DOMContentLoaded", () => {
                 let ul3A = document.createElement("ul")
                 
                 lii.appendChild(ul3A)
+                
 
                 let li1 = document.createElement("li")
                 let aa1 = document.createElement("a")
                 aa1.setAttribute("href", "A6.html")
+                aa1.classList.add("carBrand")
                 var linkTextLi1 = document.createTextNode("A8L");
                 ul3A.appendChild(li1)
-                li1.appendChild(aa1)
+                li1.appendChild(aa1)                
                 aa1.appendChild(linkTextLi1)
+
+                let thumbnailA8l = document.createElement("img");
+                thumbnailA8l.setAttribute("src", "./assets/cars-thumbnails/a8l-thumbnail.png")
+                thumbnailA8l.style.width = "12vh"
+                aa1.appendChild(thumbnailA8l)
+
 
                 let li2 = document.createElement("li")
                 let aa2 = document.createElement("a")
                 aa2.setAttribute("href", "#")
+                aa2.classList.add("carBrand")
                 var linkTextLi2 = document.createTextNode("RS 7 Sportback");
                 ul3A.appendChild(li2)
                 li2.appendChild(aa2)
                 aa2.appendChild(linkTextLi2)
 
+                let thumbnailRs7 = document.createElement("img");
+                thumbnailRs7.setAttribute("src", "./assets/cars-thumbnails/rs7sb-thumbnail.png")
+                thumbnailRs7.style.width = "12vh"
+                aa2.appendChild(thumbnailRs7)
+
                 let li3 = document.createElement("li") 
                 let aa3 = document.createElement("a")
                 aa3.setAttribute("href", "#")
+                aa3.classList.add("carBrand")
                 var linkTextLi3 = document.createTextNode("RS Q8");
                 ul3A.appendChild(li3)
                 li3.appendChild(aa3)
                 aa3.appendChild(linkTextLi3)
+
+                let thumbnailRsq8 = document.createElement("img");
+                thumbnailRsq8.setAttribute("src", "./assets/cars-thumbnails/rsq8-thumbnail.png")
+                thumbnailRsq8.style.width = "12vh"
+                aa3.appendChild(thumbnailRsq8)
 
 
                 // BMW
@@ -135,26 +155,45 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1B = document.createElement("li")
                 let aa1B = document.createElement("a")
                 aa1B.setAttribute("href", "http://www.facebook.com")
+                aa1B.classList.add("carBrand")
                 var linkTextLi1B = document.createTextNode("850i Cabrio");
                 ul3B.appendChild(li1B)
                 li1B.appendChild(aa1B)
                 aa1B.appendChild(linkTextLi1B)
 
+                let thumbnail850i = document.createElement("img");
+                thumbnail850i.setAttribute("src", "./assets/cars-thumbnails/bmw8-thumbnail.png")
+                thumbnail850i.style.width = "12vh"
+                aa1B.appendChild(thumbnail850i)
+
+
                 let li2B = document.createElement("li")
                 let aa2B = document.createElement("a")
                 aa2B.setAttribute("href", "#")
+                aa2B.classList.add("carBrand")
                 var linkTextLi2B = document.createTextNode("M5 Competition");
                 ul3B.appendChild(li2B)
                 li2B.appendChild(aa2B)
                 aa2B.appendChild(linkTextLi2B)
 
+                let thumbnailM5 = document.createElement("img");
+                thumbnailM5.setAttribute("src", "./assets/cars-thumbnails/bmwm5-thumbnail.png")
+                thumbnailM5.style.width = "12vh"
+                aa2B.appendChild(thumbnailM5)
+
                 let li3B = document.createElement("li") 
                 let aa3B = document.createElement("a")
                 aa3B.setAttribute("href", "#")
+                aa3B.classList.add("carBrand")
                 var linkTextLi3B = document.createTextNode("X6 M");
                 ul3B.appendChild(li3B)
                 li3B.appendChild(aa3B)
                 aa3B.appendChild(linkTextLi3B)
+
+                let thumbnailX6 = document.createElement("img");
+                thumbnailX6.setAttribute("src", "./assets/cars-thumbnails/bmwx6m-thumbnail.png")
+                thumbnailX6.style.width = "10.5vh"
+                aa3B.appendChild(thumbnailX6)
                 
 
                 // Mercedes Benz
@@ -184,26 +223,44 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1M = document.createElement("li")
                 let aa1M = document.createElement("a")
                 aa1M.setAttribute("href", "http://www.facebook.com")
+                aa1M.classList.add("carBrand")
                 var linkTextLi1M = document.createTextNode("GLS 63 4MATIC+");
                 ul3M.appendChild(li1M)
                 li1M.appendChild(aa1M)
                 aa1M.appendChild(linkTextLi1M)
 
+                let thumbnailGLS = document.createElement("img");
+                thumbnailGLS.setAttribute("src", "./assets/cars-thumbnails/gls-thumbnail.png")
+                thumbnailGLS.style.width = "10.5vh"
+                aa1M.appendChild(thumbnailGLS)
+
                 let li2M = document.createElement("li")
                 let aa2M = document.createElement("a")
                 aa2M.setAttribute("href", "#")
+                aa2M.classList.add("carBrand")
                 var linkTextLi2M = document.createTextNode("GT 63 S");
                 ul3M.appendChild(li2M)
                 li2M.appendChild(aa2M)
                 aa2M.appendChild(linkTextLi2M)
 
+                let thumbnailGT = document.createElement("img");
+                thumbnailGT.setAttribute("src", "./assets/cars-thumbnails/gt63s-thumbnail.png")
+                thumbnailGT.style.width = "10.5vh"
+                aa2M.appendChild(thumbnailGT)
+
                 let li3M = document.createElement("li") 
                 let aa3M = document.createElement("a")
                 aa3M.setAttribute("href", "#")
+                aa3M.classList.add("carBrand")
                 var linkTextLi3M = document.createTextNode("S 500 4MATIC");
                 ul3M.appendChild(li3M)
                 li3M.appendChild(aa3M)
                 aa3M.appendChild(linkTextLi3M)
+
+                let thumbnailS = document.createElement("img");
+                thumbnailS.setAttribute("src", "./assets/cars-thumbnails/s-thumbnail.png")
+                thumbnailS.style.width = "10.5vh"
+                aa3M.appendChild(thumbnailS)
                
 
                 // Porsche
@@ -220,7 +277,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let carLogoPorsche = document.createElement("img");
                 carLogoPorsche.setAttribute("src", "./assets/cars-logo/PORSCHE-logo.png")
-                carLogoPorsche.style.width = "3.8vh"
+                carLogoPorsche.style.width = "3.4vh"
 
                 liiP.appendChild(aaP)
                 aaP.appendChild(linkTextP)
@@ -233,18 +290,30 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1P = document.createElement("li")
                 let aa1P = document.createElement("a")
                 aa1P.setAttribute("href", "http://www.facebook.com")
+                aa1P.classList.add("carBrand")
                 var linkTextLi1P = document.createTextNode("911 Turbo S");
                 ul3P.appendChild(li1P)
                 li1P.appendChild(aa1P)
                 aa1P.appendChild(linkTextLi1P)
 
+                let thumbnail911S = document.createElement("img");
+                thumbnail911S.setAttribute("src", "./assets/cars-thumbnails/911ts-thumbnail.png")
+                thumbnail911S.style.width = "10.5vh"
+                aa1P.appendChild(thumbnail911S)
+
                 let li2P = document.createElement("li")
                 let aa2P = document.createElement("a")
                 aa2P.setAttribute("href", "#")
+                aa2P.classList.add("carBrand")
                 var linkTextLi2P = document.createTextNode("Cayenne Turbo Coupé");
                 ul3P.appendChild(li2P)
                 li2P.appendChild(aa2P)
                 aa2P.appendChild(linkTextLi2P)
+
+                let thumbnailCayS = document.createElement("img");
+                thumbnailCayS.setAttribute("src", "./assets/cars-thumbnails/cayenneS-thumbnail.png")
+                thumbnailCayS.style.width = "10.5vh"
+                aa2P.appendChild(thumbnailCayS)
 
     //Contact
     let contact = document.createElement("li")
