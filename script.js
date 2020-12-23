582131eca75d50b23c5d52bdd45f6533c2a38a92
Filip Michal Gajdoš <gajdos.filip132@gmail.com>
2020-12-23 14:08:23 +0100

    breadcrumbs finale(done)

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li1 = document.createElement("li")
     let aa1 = document.createElement("a")
-    aa1.setAttribute("href", "A6.html")
+    aa1.setAttribute("href", "A8L.html")
     var linkTextLi1 = document.createTextNode("A8L");
     ul3A.appendChild(li1)
     li1.appendChild(aa1)
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li2 = document.createElement("li")
     let aa2 = document.createElement("a")
-    aa2.setAttribute("href", "#")
+    aa2.setAttribute("href", "RS7.html")
     var linkTextLi2 = document.createTextNode("RS 7 Sportback");
     ul3A.appendChild(li2)
     li2.appendChild(aa2)
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li3 = document.createElement("li")
     let aa3 = document.createElement("a")
-    aa3.setAttribute("href", "#")
+    aa3.setAttribute("href", "RSQ8.html")
     var linkTextLi3 = document.createTextNode("RS Q8");
     ul3A.appendChild(li3)
     li3.appendChild(aa3)
@@ -138,7 +138,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li1B = document.createElement("li")
     let aa1B = document.createElement("a")
-    aa1B.setAttribute("href", "http://www.facebook.com")
+    aa1B.setAttribute("href", "850i.html")
     var linkTextLi1B = document.createTextNode("850i Cabrio");
     ul3B.appendChild(li1B)
     li1B.appendChild(aa1B)
@@ -146,7 +146,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li2B = document.createElement("li")
     let aa2B = document.createElement("a")
-    aa2B.setAttribute("href", "#")
+    aa2B.setAttribute("href", "M5.html")
     var linkTextLi2B = document.createTextNode("M5 Competition");
     ul3B.appendChild(li2B)
     li2B.appendChild(aa2B)
@@ -154,7 +154,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li3B = document.createElement("li")
     let aa3B = document.createElement("a")
-    aa3B.setAttribute("href", "#")
+    aa3B.setAttribute("href", "X6M.html")
     var linkTextLi3B = document.createTextNode("X6 M");
     ul3B.appendChild(li3B)
     li3B.appendChild(aa3B)
@@ -187,7 +187,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li1M = document.createElement("li")
     let aa1M = document.createElement("a")
-    aa1M.setAttribute("href", "http://www.facebook.com")
+    aa1M.setAttribute("href", "GLS63.html")
     var linkTextLi1M = document.createTextNode("GLS 63 4MATIC+");
     ul3M.appendChild(li1M)
     li1M.appendChild(aa1M)
@@ -195,7 +195,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li2M = document.createElement("li")
     let aa2M = document.createElement("a")
-    aa2M.setAttribute("href", "#")
+    aa2M.setAttribute("href", "GT63S.html")
     var linkTextLi2M = document.createTextNode("GT 63 S");
     ul3M.appendChild(li2M)
     li2M.appendChild(aa2M)
@@ -203,7 +203,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li3M = document.createElement("li")
     let aa3M = document.createElement("a")
-    aa3M.setAttribute("href", "#")
+    aa3M.setAttribute("href", "S500.html")
     var linkTextLi3M = document.createTextNode("S 500 4MATIC");
     ul3M.appendChild(li3M)
     li3M.appendChild(aa3M)
@@ -236,7 +236,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li1P = document.createElement("li")
     let aa1P = document.createElement("a")
-    aa1P.setAttribute("href", "http://www.facebook.com")
+    aa1P.setAttribute("href", "911TurboS.html")
     var linkTextLi1P = document.createTextNode("911 Turbo S");
     ul3P.appendChild(li1P)
     li1P.appendChild(aa1P)
@@ -244,7 +244,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let li2P = document.createElement("li")
     let aa2P = document.createElement("a")
-    aa2P.setAttribute("href", "#")
+    aa2P.setAttribute("href", "CayenneTurbo.html")
     var linkTextLi2P = document.createTextNode("Cayenne Turbo Coupé");
     ul3P.appendChild(li2P)
     li2P.appendChild(aa2P)
@@ -280,7 +280,7 @@ document.addEventListener("DOMContentLoaded", () => {
     input.setAttribute("aria-label", "Search");
 
     let btn = document.createElement("button");
-    btn.setAttribute("class", "btn btn-outline-success my-2 my-sm-0");
+    btn.setAttribute("class", "btn btn-secondary my-2 my-sm-0");
     btn.setAttribute("type", "submit");
     btn.setAttribute("data-toggle", "modal");
     btn.setAttribute("data-target", "#modal");
@@ -358,49 +358,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let ol = document.createElement("ol")
     ol.setAttribute("id", "breadcrumbs")
-    //ol.setAttribute("class", "breadcrumb")
     nav.appendChild(ol)
 
-    /*let liNavHome = document.createElement("li")
-    liNavHome.setAttribute("class", "breadcrumb-item")
-    liNavHome.setAttribute("id", "home")
-    let aNavHome = document.createElement("a")
-    aNavHome.setAttribute("href", "#")
-    var TextNavHome = document.createTextNode("Home");
-    ol.appendChild(liNavHome)
-    liNavHome.appendChild(aNavHome)
-    aNavHome.appendChild(TextNavHome)
-
-    let liNavLib = document.createElement("li")
-    liNavLib.setAttribute("class", "breadcrumb-item")
-    liNavLib.setAttribute("id", "about")
-    let aNavLib = document.createElement("a")
-    aNavLib.setAttribute("href", "#")
-    var TextNavLib = document.createTextNode("About");
-    ol.appendChild(liNavLib)
-    liNavLib.appendChild(aNavLib)
-    aNavLib.appendChild(TextNavLib)
-
-    let liNavGal = document.createElement("li")
-    liNavGal.setAttribute("class", "breadcrumb-item")
-    liNavGal.setAttribute("id", "gallery")
-    let aNavGal = document.createElement("a")
-    aNavGal.setAttribute("href", "#")
-    var TextNavGal = document.createTextNode("Gallery");
-    ol.appendChild(liNavGal)
-    liNavGal.appendChild(aNavGal)
-    aNavLiGal.appendChild(TextNavGal)
-
-    let liNavData = document.createElement("li")
-    liNavData.setAttribute("class", "breadcrumb-item active")
-    liNavData.setAttribute("id", "contact")
-    liNavData.setAttribute("aria-current", "page")
-    let aNavData = document.createElement("a")
-    aNavData.setAttribute("href", "#")
-    var TextNavData = document.createTextNode("Contact");
-    ol.appendChild(liNavData)
-    liNavData.appendChild(aNavData)
-    aNavData.appendChild(TextNavData)*/
 
     // Search names/dates
 
@@ -527,44 +486,85 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     form.addEventListener('submit', search);
-    Ahome.addEventListener('click', plz_kill_me_final());
-    //Aabout.addEventListener('click', plz_kill_me_final());
-    //Acontact.addEventListener('click', plz_kill_me_final());
-    kill_me_plz();
+    Ahome.addEventListener('click', store_breadcrumbs());
+
+    breadcrumbs();
 })
-function plz_kill_me_final() {
+
+//Breadcrumbs functions
+
+function store_breadcrumbs() {
     let url = document.referrer;
     let local = localStorage.getItem("kys");
     if (!local) {
         local = "";
-        //local[0] = "";
-        //local[1] = "";
     }
     local = local.split(',');
     local.push(url);
     localStorage.setItem("kys", local);
 }
 
-function kill_me_plz() {
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+
+function breadcrumbs() {
     let local = localStorage.getItem("kys");
     if (!local) {
         local = " ";
-        //local[0] = "";
-        //local[1] = "";
     }
     local = local.split(',');
-    if(local[0] == "" || local[0] == " ")
+    if (local[0] == "" || local[0] == " ")
         local.shift();
+    if (local.length > 5)
+        local.shift();
+    local = local.filter(onlyUnique);
+    local = local.filter(function (el) {
+        return el != null;
+    });
     let placeholder = document.getElementById("breadcrumbs");
-    let li = document.createElement("li");
     for (let i = 0; i < local.length; i++) {
         let li = document.createElement("li");
-        li.append(local[i]);
-        placeholder.append(li);
+        let a = document.createElement("a");
+        a.setAttribute("href", local[i])
+        if (local[i].includes("index"))
+            a.innerHTML = "Home";
+        else if (local[i].includes("about"))
+            a.innerHTML = "About";
+        else if (local[i].includes("contact"))
+            a.innerHTML = "Contact";
+        else if (local[i].includes("A8L"))
+            a.innerHTML = "A8L";
+        else if (local[i].includes("RS7"))
+            a.innerHTML = "RS 7 Sportback";
+        else if (local[i].includes("RSQ8"))
+            a.innerHTML = "RS Q8";
+        else if (local[i].includes("850i"))
+            a.innerHTML = "850i Cabrio";
+        else if (local[i].includes("M5"))
+            a.innerHTML = "M5 Competition";
+        else if (local[i].includes("X6M"))
+            a.innerHTML = "X6 M";
+        else if (local[i].includes("GLS63"))
+            a.innerHTML = "GLS 63 4MATIC+";
+        else if (local[i].includes("GT63S"))
+            a.innerHTML = "GT 63 S";
+        else if (local[i].includes("S500"))
+            a.innerHTML = "S 500 4MATIC";
+        else if (local[i].includes("911TurboS"))
+            a.innerHTML = "911 Turbo S";
+            else if (local[i].includes("CayenneTurbo"))
+            a.innerHTML = "Cayenne Turbo Coupé";
+        else
+            a.innerHTML = "Home";
+        if (i != 0) {
+            li.setAttribute("class", "notFirst");
+        }
+        else {
+            li.setAttribute("class", "");
+        }
+        li.appendChild(a);
+        placeholder.appendChild(li);
     }
     localStorage.setItem("kys", local);
-
-
-    console.log("local=" + local);
-    console.log(localStorage.getItem("kys"));
 }
