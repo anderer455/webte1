5ce870984efcda3f27c15b785f19d423e1b2da45
AdamT <dubst3pdnb@gmail.com>
2020-12-23 02:48:06 +0100

    car detail page

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let aa = document.createElement("a")
                 aa.setAttribute("href", "#")
-                aa.classList.add("carBrand")
+                aa.classList.add("menuCarBrand")
                 var linkText = document.createTextNode("Audi");
 
                 let carLogoAudi = document.createElement("img");
@@ -85,8 +85,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let li1 = document.createElement("li")
                 let aa1 = document.createElement("a")
-                aa1.setAttribute("href", "A6.html")
-                aa1.classList.add("carBrand")
+                aa1.setAttribute("href", "A8L.html")
+                aa1.classList.add("menuCarBrand")
                 var linkTextLi1 = document.createTextNode("A8L");
                 ul3A.appendChild(li1)
                 li1.appendChild(aa1)                
@@ -101,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li2 = document.createElement("li")
                 let aa2 = document.createElement("a")
                 aa2.setAttribute("href", "#")
-                aa2.classList.add("carBrand")
+                aa2.classList.add("menuCarBrand")
                 var linkTextLi2 = document.createTextNode("RS 7 Sportback");
                 ul3A.appendChild(li2)
                 li2.appendChild(aa2)
@@ -115,7 +115,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li3 = document.createElement("li") 
                 let aa3 = document.createElement("a")
                 aa3.setAttribute("href", "#")
-                aa3.classList.add("carBrand")
+                aa3.classList.add("menuCarBrand")
                 var linkTextLi3 = document.createTextNode("RS Q8");
                 ul3A.appendChild(li3)
                 li3.appendChild(aa3)
@@ -136,7 +136,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let aaB = document.createElement("a")
                 aaB.setAttribute("href", "#")
-                aaB.classList.add("carBrand")
+                aaB.classList.add("menuCarBrand")
                 var linkTextB = document.createTextNode("BMW");
 
                 let carLogoBmw = document.createElement("img");
@@ -155,7 +155,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1B = document.createElement("li")
                 let aa1B = document.createElement("a")
                 aa1B.setAttribute("href", "http://www.facebook.com")
-                aa1B.classList.add("carBrand")
+                aa1B.classList.add("menuCarBrand")
                 var linkTextLi1B = document.createTextNode("850i Cabrio");
                 ul3B.appendChild(li1B)
                 li1B.appendChild(aa1B)
@@ -170,7 +170,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li2B = document.createElement("li")
                 let aa2B = document.createElement("a")
                 aa2B.setAttribute("href", "#")
-                aa2B.classList.add("carBrand")
+                aa2B.classList.add("menuCarBrand")
                 var linkTextLi2B = document.createTextNode("M5 Competition");
                 ul3B.appendChild(li2B)
                 li2B.appendChild(aa2B)
@@ -184,7 +184,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li3B = document.createElement("li") 
                 let aa3B = document.createElement("a")
                 aa3B.setAttribute("href", "#")
-                aa3B.classList.add("carBrand")
+                aa3B.classList.add("menuCarBrand")
                 var linkTextLi3B = document.createTextNode("X6 M");
                 ul3B.appendChild(li3B)
                 li3B.appendChild(aa3B)
@@ -205,7 +205,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let aaM = document.createElement("a")
                 aaM.setAttribute("href", "#")
-                aaM.classList.add("carBrand")
+                aaM.classList.add("menuCarBrand")
                 var linkTextM = document.createTextNode("Mercedes Benz");
 
                 let carLogoMb = document.createElement("img");
@@ -223,7 +223,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1M = document.createElement("li")
                 let aa1M = document.createElement("a")
                 aa1M.setAttribute("href", "http://www.facebook.com")
-                aa1M.classList.add("carBrand")
+                aa1M.classList.add("menuCarBrand")
                 var linkTextLi1M = document.createTextNode("GLS 63 4MATIC+");
                 ul3M.appendChild(li1M)
                 li1M.appendChild(aa1M)
@@ -237,7 +237,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li2M = document.createElement("li")
                 let aa2M = document.createElement("a")
                 aa2M.setAttribute("href", "#")
-                aa2M.classList.add("carBrand")
+                aa2M.classList.add("menuCarBrand")
                 var linkTextLi2M = document.createTextNode("GT 63 S");
                 ul3M.appendChild(li2M)
                 li2M.appendChild(aa2M)
@@ -251,7 +251,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li3M = document.createElement("li") 
                 let aa3M = document.createElement("a")
                 aa3M.setAttribute("href", "#")
-                aa3M.classList.add("carBrand")
+                aa3M.classList.add("menuCarBrand")
                 var linkTextLi3M = document.createTextNode("S 500 4MATIC");
                 ul3M.appendChild(li3M)
                 li3M.appendChild(aa3M)
@@ -272,7 +272,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let aaP = document.createElement("a")
                 aaP.setAttribute("href", "#")
-                aaP.classList.add("carBrand")
+                aaP.classList.add("menuCarBrand")
                 var linkTextP = document.createTextNode("Porsche");
 
                 let carLogoPorsche = document.createElement("img");
@@ -290,7 +290,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li1P = document.createElement("li")
                 let aa1P = document.createElement("a")
                 aa1P.setAttribute("href", "http://www.facebook.com")
-                aa1P.classList.add("carBrand")
+                aa1P.classList.add("menuCarBrand")
                 var linkTextLi1P = document.createTextNode("911 Turbo S");
                 ul3P.appendChild(li1P)
                 li1P.appendChild(aa1P)
@@ -304,7 +304,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let li2P = document.createElement("li")
                 let aa2P = document.createElement("a")
                 aa2P.setAttribute("href", "#")
-                aa2P.classList.add("carBrand")
+                aa2P.classList.add("menuCarBrand")
                 var linkTextLi2P = document.createTextNode("Cayenne Turbo Coupé");
                 ul3P.appendChild(li2P)
                 li2P.appendChild(aa2P)
@@ -354,18 +354,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Date/Time
 
+    let topDiv = document.getElementById("dateTimeName")
     let date = document.createElement("p");
     date.setAttribute("id","datetime");
-    form.appendChild(date);
+    topDiv.appendChild(date);
     function date_time() {
         var dt = new Date();
         document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "." + (("0" + (dt.getMonth() + 1)).slice(-2)) + "." + (dt.getFullYear()) + " " + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2));
     }
     const interval = setInterval(date_time(), 1000);
+    
     //Name Day
     let name_day = document.createElement("p");
     name_day.setAttribute("id","name_day");
-    form.appendChild(name_day);
+    
+    topDiv.appendChild(name_day);
 
     function loadXMLDoc(dname) {
         if (window.XMLHttpRequest) {
